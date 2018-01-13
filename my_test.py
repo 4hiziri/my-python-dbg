51e6077e90150213dbc61d6fb2c4935babf6bcd4
4hiziri <meirvg@gmail.com>
2018-01-13 11:40:11 +0900

    Add reg get demo

--- my_test.py
+++ my_test.py
@@ -6,5 +6,19 @@ dbg = mydbg.Debugger()
 pid = input("Enter PID: ")
 
 dbg.attach(int(pid))
-dbg.run()
+threads = dbg.enumerate_threads()
+
+for thread in threads:
+    thread_context = dbg.get_thread_context(thread)
+    print('-----------------------------------')
+    print("TID: {}".format(thread))
+    print("EIP: {}".format(thread_context.Eip))
+    print("ESP: {}".format(thread_context.Esp))
+    print("EBP: {}".format(thread_context.Ebp))
+    print("EAX: {}".format(thread_context.Eax))
+    print("EBX: {}".format(thread_context.Ebx))
+    print("ECX: {}".format(thread_context.Ecx))
+    print("EDX: {}".format(thread_context.Edx))
+    print('-----------------------------------')
+
 dbg.detach()
