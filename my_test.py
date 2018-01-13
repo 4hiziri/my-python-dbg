ace489257db3bf1afdceba77ee00fe5d74ab80c7
4hiziri <meirvg@gmail.com>
2018-01-13 10:19:56 +0900

    Add test

--- my_test.py
+++ my_test.py
@@ -1,4 +1,10 @@
 import mydbg
 
 dbg = mydbg.Debugger()
-dbg.load(b"C:\\Windows\\System32\\calc.exe")
+# dbg.load(b"C:\\Windows\\System32\\calc.exe")
+
+pid = input("Enter PID: ")
+
+dbg.attach(int(pid))
+dbg.run()
+dbg.detach()
