1310bad5b8af245ddbc0c31590744361a2df3c19
4hiziri <meirvg@gmail.com>
2018-01-15 15:39:47 +0900

    Add exception handler

--- mydbg.py
+++ mydbg.py
@@ -11,6 +11,7 @@ class Debugger:
         self.context = None
         self.debugger_active = False
         self.pid = int(-1)
+        self.exception_address = None
 
     def load(self, debuggee_path):
         creation_flags = DEBUG_PROCESS
@@ -64,6 +65,19 @@ class Debugger:
             self.context = self.get_thread_context(h_thread=self.h_thread)
 
             print("Event Code = {}, Thread ID = {}".format(debug_event.dwDebugEventCode, debug_event.dwThreadId))
+
+            if debug_event.dwDebugEvntCode == EXCEPTION_DEBUG_EVENT:
+                exception = debug_event.u.Exception.ExceptionRecord.ExceptionCode
+                self.exception_address = debug_event.u.Exception.ExceptionRecord.ExceptionAddress
+
+                if exception == EXCEPTION_ACCESS_VIOLATION:
+                    print("Access Violation Detected")
+                elif exception == EXCEPTION_BREAKPOINT:
+                    continue_status = self.exception_handler_breakpoint()
+                elif exception == EXCEPTION_GUARD_PAGE:
+                    print("Guard Page Access Detected")
+                elif exception == EXCEPTION_SINGLE_STEP:
+                    print("Signal stepping")
 
             kernel32.ContinueDebugEvent(debug_event.dwProcessId,
                                         debug_event.dwThreadId,
@@ -119,3 +133,8 @@ class Debugger:
             return context
         else:
             return None
+
+    def exception_handler_breakpoint(self):
+        print("Inside breakpoint handler")
+        print("Exception addr: 0x{:X}".format(self.exception_address))
+        return DBG_CONTINUE
